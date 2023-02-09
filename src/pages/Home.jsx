af26c47fbdae451dbc593d58e86cafe6d4a12ba0
PauloScapol <paulo.scapol2@hotmail.com>
2023-02-09 17:59:20 -0300

    -Requisito 4

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,36 @@
 import React, { Component } from 'react';
+import Categories from '../components/Categories';
+import * as api from '../services/api';
 
 class Home extends Component {
   state = {
     productList: [],
   };
 
+  componentDidMount() {
+    api.getCategories().then((categories) => {
+      this.setState({ productList: categories });
+    });
+  }
+
   render() {
     const { productList } = this.state;
 
     return (
-      <div data-testid="home-initial-message">
-        <input type="text" />
+      <>
+        <div>
+          <Categories categories={ productList } />
+        </div>
 
-        {productList.length === 0 ? (
-          <h2>
-            Digite algum termo de pesquisa ou escolha uma categoria.
-          </h2>) : undefined}
-      </div>
+        <div data-testid="home-initial-message">
+          <input type="text" />
+
+          {productList.length === 0 ? (
+            <h2>
+              Digite algum termo de pesquisa ou escolha uma categoria.
+            </h2>) : undefined}
+        </div>
+      </>
     );
   }
 }
